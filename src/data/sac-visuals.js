b7ef310dd2deb829cc58d56f25d937ee0b557a0e
gprenveille4d7-ux <g.prenveille4d7@gmail.com>
2026-07-20 15:31:26 +0200

    Ajoute les vues des sacs bleu et vert

--- src/data/sac-visuals.js
+++ src/data/sac-visuals.js
@@ -49,3 +49,88 @@ export function sacRougeViewForSection(sectionId) {
 }
 
 export const SAC_ROUGE_PRELOAD_IMAGES = Object.freeze([...new Set(Object.values(SAC_ROUGE_VIEWS))]);
+
+const SAC_BLEU_ASSET_ROOT = './assets/sacs/sac-bleu';
+const SAC_VERT_ASSET_ROOT = './assets/sacs/sac-vert';
+
+export const BAG_PHOTO_CONFIGS = Object.freeze({
+  [SAC_ROUGE_CONTAINER_ID]: Object.freeze({
+    name: 'sac rouge',
+    caption: 'Sac rouge · Vue générale',
+    views: SAC_ROUGE_VIEWS,
+    labels: SAC_ROUGE_VIEW_LABELS,
+    sectionViews: SAC_ROUGE_SECTION_VIEWS
+  }),
+  'sac-bleu-respi': Object.freeze({
+    name: 'sac bleu',
+    caption: 'Sac bleu · Vue générale',
+    views: Object.freeze({
+      face: `${SAC_BLEU_ASSET_ROOT}/sac-bleu-face.png`,
+      gauche: `${SAC_BLEU_ASSET_ROOT}/sac-bleu-cote-gauche.png`,
+      droite: `${SAC_BLEU_ASSET_ROOT}/sac-bleu-cote-droit.png`,
+      dos: `${SAC_BLEU_ASSET_ROOT}/sac-bleu-dos.png`,
+      ouvert: `${SAC_BLEU_ASSET_ROOT}/sac-bleu-ouvert.png`
+    }),
+    labels: Object.freeze({
+      face: 'Sac bleu vu de face',
+      gauche: 'Sac bleu vu du côté gauche',
+      droite: 'Sac bleu vu du côté droit',
+      dos: 'Sac bleu vu de dos',
+      ouvert: 'Sac bleu ouvert'
+    }),
+    sectionViews: Object.freeze({
+      'intubation-gauche': 'ouvert',
+      'intubation-centre': 'ouvert',
+      'intubation-droit': 'ouvert',
+      interne: 'ouvert',
+      'lateral-droit': 'droite',
+      'lateral-gauche': 'gauche'
+    })
+  }),
+  'sac-vert-pedia': Object.freeze({
+    name: 'sac vert',
+    caption: 'Sac vert · Vue générale',
+    views: Object.freeze({
+      face: `${SAC_VERT_ASSET_ROOT}/sac-vert-face.png`,
+      gauche: `${SAC_VERT_ASSET_ROOT}/sac-vert-cote-gauche.png`,
+      droite: `${SAC_VERT_ASSET_ROOT}/sac-vert-cote-droit.png`,
+      dos: `${SAC_VERT_ASSET_ROOT}/sac-vert-dos.png`,
+      ouvert: `${SAC_VERT_ASSET_ROOT}/sac-vert-ouvert.png`
+    }),
+    labels: Object.freeze({
+      face: 'Sac vert vu de face',
+      gauche: 'Sac vert vu du côté gauche',
+      droite: 'Sac vert vu du côté droit',
+      dos: 'Sac vert vu de dos',
+      ouvert: 'Sac vert ouvert'
+    }),
+    sectionViews: Object.freeze({
+      ampoulier: 'ouvert',
+      'kit-perfusion': 'ouvert',
+      'kit-paracetamol': 'ouvert',
+      'oxygene-aerosol': 'ouvert',
+      'plaque-a': 'ouvert',
+      'plaque-b': 'ouvert',
+      intubation: 'ouvert',
+      fond: 'ouvert',
+      'sac-bleu': 'ouvert',
+      'lateral-droit': 'droite',
+      'lateral-gauche': 'gauche'
+    })
+  })
+});
+
+export function bagPhotoConfig(containerId) {
+  return BAG_PHOTO_CONFIGS[containerId] || null;
+}
+
+export function bagViewForSection(containerId, sectionId) {
+  const config = bagPhotoConfig(containerId);
+  const token = String(sectionId || '').split(':').at(-1);
+  return config?.sectionViews[token] || 'face';
+}
+
+export function bagPreloadImages(containerId) {
+  const config = bagPhotoConfig(containerId);
+  return config ? [...new Set(Object.values(config.views))] : [];
+}
