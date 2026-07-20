f82aa66cc95f951abfb017bba20338ebf3113291
gprenveille4d7-ux <g.prenveille4d7@gmail.com>
2026-07-20 18:26:24 +0200

    Force le chargement de la nouvelle version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'releve-smur-';
-const CACHE = 'releve-smur-v1.0.0-20260720-plaque-face-b';
+const CACHE = 'releve-smur-v1.0.0-20260720-plaque-face-b-v2';
 const NETWORK_TIMEOUT_MS = 4000;
 const CORE_ASSETS = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg',
